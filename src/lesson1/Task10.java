0de943b4bd3a05bf3e689cddc4f969f23bf9681a
DmitryGhoncharko <70809609+DmitryGhoncharko@users.noreply.github.com>
2022-06-22 14:30:04 +0300

    Update Task10.java

--- src/lesson1/Task10.java
+++ src/lesson1/Task10.java
@@ -7,7 +7,7 @@ package lesson1;
 public class Task10 {
     public static  int number = 999999999;
     public static void main(String[] args) {
-
+        // you need to use long instead of int, since we have an int(32 bit) overflow
         int square_number = number*number;
         System.out.println(square_number);
     }
